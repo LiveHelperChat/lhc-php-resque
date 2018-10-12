5bb0f8b57117cb19d28891bb22ecf1c4ddb30fbb
Remigijus Kiminas <remdex@gmail.com>
2018-10-12 08:49:32 -0400

    Make paths more easier to understand

--- lhcphpresque/doc/phpresque.sh
+++ lhcphpresque/doc/phpresque.sh
@@ -3,4 +3,4 @@
 
 echo "Running live site cronjobs"
 cd /var/www/web
-REDIS_BACKEND=localhost:6379 INTERVAL=5 REDIS_BACKEND_DB=1 COUNT=4 VERBOSE=0 QUEUE='*' /usr/bin/php resque.php 2>&1
+REDIS_BACKEND=localhost:6379 INTERVAL=5 REDIS_BACKEND_DB=0 COUNT=4 VERBOSE=0 QUEUE='*' /usr/bin/php resque.php 2>&1
--- lhcphpresque/doc/resque.sh
+++ lhcphpresque/doc/resque.sh
@@ -7,23 +7,23 @@ if (( ${uptime_secs} < 120 )); then
     exit 1
 fi
 
-fileCron='/var/www/web/cron/.enable-cron'
+fileCron='/var/www/web/extension/lhcphpresque/doc/.enable-cron'
 
 if [ -f $fileCron ];
 then
 
-fileLock="/var/www/web/cron/runresque.lock"
+fileLock="/var/www/web/extension/lhcphpresque/doc/runresque.lock"
 
 if [ -f $fileLock ];
 then
     kill -9 $(ps aux | grep "[0-9] resque-1.2: *" | awk '{print $2}')
-    cd /var/www/web/cron/ && ./phpresque.sh
+    cd /var/www/web/extension/lhcphpresque/doc/ && ./phpresque.sh
     rm -f $fileLock;
 else
     PIDS=`ps aux | grep '[0-9] resque-1.2: *'`
     if [ -z "$PIDS" ]; then
        echo "Starting resque"
-       cd /var/www/web/cron/ && ./phpresque.sh
+       cd /var/www/web/extension/lhcphpresque/doc/ && ./phpresque.sh
     fi
 fi
 
