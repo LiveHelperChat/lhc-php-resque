0bf76798689cfd9032c93569ede471cdd0a99474
Remigijus Kiminas <remdex@gmail.com>
2020-05-06 06:39:23 -0400

    Sometiems background jobs needs to know base url where lhc is installed.

--- lhcphpresque/settings/settings.ini.default.php
+++ lhcphpresque/settings/settings.ini.default.php
@@ -4,8 +4,10 @@ return array(
     'connect_resque' => 'localhost',
     'connect_resque_db' => 1,
     'automated_hosting' => false,
+    'site_address' => 'https://devmysql.livehelperchat.com/lhc_web/index.php',
     'queues' => array(
-        'lhc_dummy_queue'
+        'lhc_dummy_queue',
+        'lhc_rest_api_queue',
     )
 );
 
