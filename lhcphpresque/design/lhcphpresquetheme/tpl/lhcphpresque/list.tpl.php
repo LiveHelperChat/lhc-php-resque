cf0b0a1f36e3c1f4e1c6b5d1cc839eee3d02dba9
Remigijus Kiminas <remdex@gmail.com>
2021-06-21 09:14:17 -0400

    Escape template

--- lhcphpresque/design/lhcphpresquetheme/tpl/lhcphpresque/list.tpl.php
+++ lhcphpresque/design/lhcphpresquetheme/tpl/lhcphpresque/list.tpl.php
@@ -1,6 +1,6 @@
 <h1>List</h1>
 <form action="<?php echo erLhcoreClassDesign::baseurl('lhcphpresque/list')?>/<?php echo $list?>/" method="post">
-<table class="table" cellpadding="0" cellspacing="0" width="100%">
+<table class="table" cellpadding="0" cellspacing="0" width="100%" ng-non-bindable>
 	<thead>
 	<tr>
 	    <th width="1%">ID</th>
