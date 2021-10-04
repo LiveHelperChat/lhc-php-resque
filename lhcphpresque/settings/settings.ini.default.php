ea3910ba931f32e822b5c8910faae8facf07a372
Federico Piazzon <10348142+elplaza@users.noreply.github.com>
2021-10-04 10:17:40 +0200

    Update settings.ini.default.php
    Replace values with environment variables

--- lhcphpresque/settings/settings.ini.default.php
+++ lhcphpresque/settings/settings.ini.default.php
@@ -1,11 +1,11 @@
 <?php 
 
 return array(
-    'connect_resque' => 'localhost',
+    'connect_resque' => getenv('RESQUE_HOST') ?: 'localhost',
     'connect_resque_db' => 1,
     'automated_hosting' => false,
     'uac' => false,
-    'site_address' => 'https://example.org/lhc_web/index.php',
+    'site_address' => getenv('RESQUE_SITEADDRESS') ?: 'https://example.org/lhc_web/index.php',
     'queues' => array(
         'lhc_dummy_queue',
         'lhc_rest_api_queue',
