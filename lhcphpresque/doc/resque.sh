ebca8006a451452d30a6170d9d4bd81ef9c20bde
Remigijus Kiminas <remdex@gmail.com>
2024-08-27 01:11:52 -0400

    Support for new process management

--- lhcphpresque/doc/resque.sh
+++ lhcphpresque/doc/resque.sh
@@ -24,14 +24,15 @@ fileLock="/var/www/web/extension/lhcphpresque/doc/runresque.lock"
 
 if [ -f $fileLock ];
 then
+    kill -9 $(ps aux | grep "php resque.php" | awk '{print $2}')
     kill -9 $(ps aux | grep "[0-9] resque-1.2: *" | awk '{print $2}')
-    cd /var/www/web/extension/lhcphpresque/doc/ && ./phpresque.sh
+    cd /var/www/web/extension/lhcphpresque/doc/ && ./phpresque.sh &
     rm -f $fileLock;
 else
     PIDS=`ps aux | grep '[0-9] resque-1.2: *'`
     if [ -z "$PIDS" ]; then
        echo "Starting resque"
-       cd /var/www/web/extension/lhcphpresque/doc/ && ./phpresque.sh
+       cd /var/www/web/extension/lhcphpresque/doc/ && ./phpresque.sh &
     fi
 fi
 
